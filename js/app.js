193888e06777fb76e2f78c8b82892664cff42b3e
moazhamdy22 <moaz.hamdy1223@gmail.com>
2021-11-04 16:57:08 +0200

    penultimate version

--- js/app.js
+++ js/app.js
@@ -22,7 +22,91 @@
  * Define Global Variables
  * 
 */
+// my all program Object
+let myObject = {
+    // navigation gloabal var
+    navigation : document.getElementById('navbar__list'),
+    // sections global var
+    sections : document.querySelectorAll('section'),
+    navBuilder : ()=>{
+        let navUI = '';
+        // looping over all sections
+        myObject.sections.forEach((section) => {
+            const sectionId = section.id;
+            const sectionDataNav = section.dataset.nav;
 
+            navUI += `<li><a class="menu__link ${sectionId}" href ="#${sectionId}">${sectionDataNav}</a></li>`;
+            
+        });
+        // append all elements to the navigation
+        myObject.navigation.innerHTML = navUI;
+
+    },
+    // getting the largest value that's less or equal to the number
+    offset:(section)=>{
+        // console.log(Math.floor(section.getBoundingClientRect().top));
+        return Math.floor(section.getBoundingClientRect().top);
+    },
+    // remove the active class
+    removeActive:(section) =>{
+        section.classList.remove('your-active-class');
+        document.querySelector(`.${section.id}`).style.background= "";
+    },
+    // add the active class
+    addActive:(conditional, section)=>{
+        if(conditional){
+            section.classList.add('your-active-class');
+            document.querySelector(`.${section.id}`).style.background= "#d9534f";
+        };
+
+    },
+    //implementing the actual function
+    sectionActivation:()=>{
+        myObject.sections.forEach((section) =>{
+            const elementOffset = myObject.offset(section);
+
+            let inviewport = () => elementOffset <357 && elementOffset >=-150;
+
+            myObject.removeActive(section);
+            myObject.addActive(inviewport(),section);
+        });
+    },
+    // Scroll to anchor ID using scrollTo event
+    scrolling:()=>{
+
+        const links = document.querySelectorAll('.menu__link');
+        links.forEach((link) =>{
+
+    //         document.getElementById('navbar__menu').addEventListener('click', function (link) {
+    //             link.preventDefault();
+    //             const target = link.target;
+    //             if (target.classList.contains('menu__link')) {
+    //                 const id = target.getAttribute('href').slice(1);
+    //                 document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+    //             }
+    //         }
+    //     }
+    });
+    //     //     link.addEventListener('click',()=>{
+    //     //         // for(let i = 0;i<myObject.sections.length;i++){
+    //     //         //     myObject.sections[i].document.addEventListener("click",myObject.sectionScroll(link));
+    //     //         //     console.log(myObject.sections[i])
+    //     //         // };
+    //     //         // console.log(link.getAttribute("href"));
+    //     //         document.querySelector(link.getAttribute("href")).scrollIntoView({behavior: "smooth"});
+
+    //     //     });
+    //     // });
+
+},
+
+
+}
+myObject.navBuilder();
+
+window.addEventListener('scroll',myObject.sectionActivation);
+
+myObject.scrolling();
 
 /**
  * End Global Variables
@@ -42,6 +126,7 @@
 
 
 // Add class 'active' to section when near top of viewport
+
 
 
 // Scroll to anchor ID using scrollTO event
